d3c2eb41c37f0fce30cf045a020aa89e2d71ebd0
caipaing <caipaingenieriaprueba@gmail.com>
2022-02-21 22:24:50 -0500

    update styles and register product

--- src/app/pages/products/productcreate/productcreate.component.ts
+++ src/app/pages/products/productcreate/productcreate.component.ts
@@ -10,7 +10,7 @@ import { Products } from 'src/app/shared/models/products';
 @Component({
   selector: 'app-productcreate',
   templateUrl: './productcreate.component.html',
-  styleUrls: ['./productcreate.component.scss']
+  styleUrls: ['./productcreate.component.scss'],
 })
 export class ProductcreateComponent implements OnInit {
   valueitem: any = null;
@@ -18,111 +18,116 @@ export class ProductcreateComponent implements OnInit {
   productsForm!: FormGroup;
   isOverDrop = false;
   urlImagen: any[] = [];
-  name!: string ;
-  finImage: any[] = [] ;
-  pathImage = "../../../assets/img/cloud.png";
+  name!: string;
+  finImage: any[] = [];
+  pathImage = '../../../assets/img/cloud.png';
   public isNumber = /^([0-9]){1-20}$/;
 
   navigationextras: NavigationExtras = {
     state: {
-      value: null
-    }
+      value: null,
+    },
   };
 
-
-
-
-  constructor(private router: Router,
-              public storage: AngularFireStorage,
-              public productService: ProductsService,
-              public formbuild: FormBuilder,
-              public uploadImage: UploadImageService) {
+  constructor(
+    private router: Router,
+    public storage: AngularFireStorage,
+    public productService: ProductsService,
+    public formbuild: FormBuilder,
+    public uploadImage: UploadImageService
+  ) {
     const navigation = this.router.getCurrentNavigation();
     this.valueitem = navigation?.extras?.state;
-    let countImage = Object.values(this.valueitem.imageProduct).length;
-    let archivos =  Object.values(this.valueitem.imageProduct);
-    console.log(archivos)
-    for (let i = 0; i < countImage; i++) {
-      this.finImage[i] = archivos[i];
-      this.urlImagen[i] = archivos[i];
-    }
-    
-    alert(this.finImage)
     this.CreateProductForm();
-    
   }
 
-
-  onUpload(e: any){
-   this.name = e.target.value;   
+  onUpload(e: any) {
+    this.name = e.target.value;
     console.log(this.name);
   }
-  onUploadImage(event: any){
-    
+  onUploadImage(event: any) {
     let archivos = event.target.files;
-    for (let i = 0; i < archivos.length; i++) {     
-    
-    let reader = new FileReader();
-    console.log(this.uploadImage+"\n");
-    reader.readAsDataURL(archivos[i]);
-    reader.onloadend = () => {
-      this.finImage.push(reader.result);
-      this.uploadImage.createImage(this.name, reader.result).then(urlImage =>{
-        this.urlImagen[i] = urlImage;
-        console.log(urlImage + "\n"+this.urlImagen[i]);
-      });
-      console.log(this.uploadImage+"\n");
+    for (let i = 0; i < archivos.length; i++) {
+      let reader = new FileReader();
+      console.log(this.uploadImage + '\n');
+      reader.readAsDataURL(archivos[i]);
+      reader.onloadend = () => {
+        this.finImage.push(reader.result);
+        this.uploadImage
+          .createImage(this.name, reader.result)
+          .then((urlImage) => {
+            this.urlImagen[i] = urlImage;
+            console.log(urlImage + '\n' + this.urlImagen[i]);
+          });
+        console.log(this.uploadImage + '\n');
+      };
     }
-  }
 
     console.log(event.target.files);
   }
-  onCreate(): void{
-     this.router.navigate(['productcreate']);
-   }
-  onGoToDelete(valueitem: Products): void{
-   this.productService.deleteProductList( valueitem.Id!);
-   console.log('has been deleted succesfull' + this.productService.deleteProductList(valueitem.Id!));
+  onCreate(): void {
+    this.router.navigate(['productcreate']);
   }
-  onGoToBack(): void{
+  onGoToDelete(valueitem: Products): void {
+    this.productService.deleteProductList(valueitem.Id!);
+    console.log(
+      'has been deleted succesfull' +
+        this.productService.deleteProductList(valueitem.Id!)
+    );
+  }
+  onGoToBack(): void {
     this.router.navigate(['productlist']);
-    }
-  public CreateProductForm(): void{
-     this.productsForm = this.formbuild.group({
-       name: ['', [Validators.required]],
-       tipo: ['', [Validators.required]],
-       price: ['',[Validators.required, Validators.pattern(this.isNumber)]],
-       quantity: ['',[Validators.required, Validators.pattern(this.isNumber)]],
-       units: [''],
-       description: ['', [Validators.required]],
-       imageProduct: [[]] 
-     });
-   }
+  }
+  public CreateProductForm(): void {
+    this.productsForm = this.formbuild.group({
+      name: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      price: ['', [Validators.required, Validators.pattern(this.isNumber)]],
+      quantity: ['', [Validators.required, Validators.pattern(this.isNumber)]],
+      units: [''],
+      description: ['', [Validators.required]],
+      imageProduct: [[]],
+    });
+  }
 
-  ngOnInit(): void{
-   if (typeof  this.valueitem === 'undefined'){
-       this.router.navigate(['productcreate']);
-     }else{
-       //this.urlImagen = this.valueitem.imageProduct.value;
-       this.productsForm.patchValue(this.valueitem);
-     }
+  ngOnInit(): void {
+    if (typeof this.valueitem === 'undefined') {
+      this.router.navigate(['productcreate']);
+    } else {
+      //this.urlImagen = this.valueitem.imageProduct.value;
+      let countImage = Object.values(this.valueitem.imageProduct).length;
+      let archivos = Object.values(this.valueitem.imageProduct);
+      for (let i = 0; i < countImage; i++) {
+        this.finImage[i] = archivos[i];
+        this.urlImagen[i] = archivos[i];
+      }
+      
+      console.log(archivos);
+  
+      alert(this.finImage);
+      this.productsForm.patchValue(this.valueitem);
+    }
   }
 
   onSubmit() {
-    for (let i = 0; i < this.urlImagen.length; i++) { 
-    this.productsForm.value.imageProduct[i] = this.urlImagen[i];
-    console.log(this.productsForm.valid+"\nimagen"+i +" " +this.productsForm.value.imageProduct)
+    for (let i = 0; i < this.urlImagen.length; i++) {
+      this.productsForm.value.imageProduct[i] = this.urlImagen[i];
+      console.log(
+        this.productsForm.valid +
+          '\nimagen' +
+          i +
+          ' ' +
+          this.productsForm.value.imageProduct
+      );
     }
     if (!this.productsForm.valid) {
-      console.log(this.productsForm.value)
+      console.log(this.productsForm.value);
       const product = this.productsForm.value;
       const productId = this.valueitem?.Id || null;
-      this.productService.insertProductList(product, productId!)
+      this.productService.insertProductList(product, productId!);
       this.resetForm(this.productsForm);
-      this.router.navigate(['productlist'])
+      this.router.navigate(['productlist']);
     }
-
-
   }
   resetForm(productsForm: FormGroup): void {
     if (productsForm != null) {
@@ -130,8 +135,4 @@ export class ProductcreateComponent implements OnInit {
       //this.productService.selectproduct =  this.valueitem;
     }
   }
-
 }
-
-
-
